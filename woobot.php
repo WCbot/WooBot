04adc021c9d71accb295c131bb4182035072b5df
Thomas Roberts <arthomas@arkrowd.eu>
2018-10-30 13:29:56 +0000

    Added error handling for unrecognised commands in woobot.php

--- woobot.php
+++ woobot.php
@@ -1,5 +1,5 @@
 <?php
-if ($_POST['text'] == "test")
+if ($_POST['text'] == "test") {
 	$response_array = array(
 	"username" => "WooBot",
 	"channel" => $_POST['channel_id'],
@@ -7,7 +7,16 @@ if ($_POST['text'] == "test")
 	"text" => "Hello, WooBot here, I'm working!",
 	"mrkdwn" => false,
 );
-
+} else {
+	$response_array = array(
+	"username" => "WooBot",
+	"channel" => $_POST['channel_id'],
+	"response_type" => "ephemeral"
+	"text" => "Sorry, command not recognised",
+	"mrkdwn" => false,
+);
+}
+ 
 $response_json = json_encode($response_array);
 
 header('content-type: application/json');

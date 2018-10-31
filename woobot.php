4db33aa56e9c323b6a14fdd9dd8a3710298b8118
Thomas Roberts <arthomas@arkrowd.eu>
2018-10-31 12:51:09 +0000

    add missing semicolon in woobot.php

--- woobot.php
+++ woobot.php
@@ -3,7 +3,7 @@ $signingsecret = "d359026511347aa1caffacb200c332a8";
 $basestring = "v0" . ":" . $_SERVER['HTTP_X_SLACK_REQUEST_TIMESTAMP'] . ":" . file_get_contents('php://input');
 $mysignature = "v0=" . hash_hmac("sha256", $basestring, $signingsecret);
 if ($mysignature == $_SERVER['HTTP_X_SLACK_SIGNATURE']) {
-	$textarray = explode(" ", urldecode($POST['text']))
+	$textarray = explode(" ", urldecode($POST['text']));
 	if ($textarray[0] == "test") {
 		$response_array = array(
 		"username" => "WooBot",
